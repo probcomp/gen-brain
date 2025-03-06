1d3e0af40d6663b6984047feefa0b147d6418947
larrylegend33 <abolton@mit.edu>
2025-03-06 11:19:12 -0500

    finished tests for new genjax, starting pixelmap

--- packages/demos/3dot-smc/notebooks/tests.py
+++ packages/demos/3dot-smc/notebooks/tests.py
@@ -93,3 +93,35 @@ assert(not (prop_probs_20_arg_no_constraint == prop_probs_xyz_ego_constrained).a
 
 
 # one issue here is that the interpreter cares about the order of the proposal. the order is different in the proposal switch combinator -- if it goes down the vis path, you go obs->latents. if it goes the other way, you go markov. i think this might be a problem. better to keep the model the way it is and fix the ordering issue in the interpreter (i.e. provide dep maps for both switch outcomes, which is not a huge deal). 
+
+
+# Test ability to read model metadata with new catprobs structure (i.e. you don't need to specify subtraced anymore -- only the tuple variables). 
+
+# i think you should add "how is this coming back? is it a distribution or a probability map". will no longer have to segregate between latents and observations. 
+
+# will be enjoyable to think about this over a beer tonight. write out exactly what you need to happen in each case. 
+
+# make a new ParallelBernoulli class. 
+
+model_variables = [
+    {"variable": "lights", "parents": [], "support": model.bool_support},
+    {"variable": "diam", "parents": [], "support": model.diams}, 
+    {"variable": "v3d", "parents": [], "support": model.xyz_vels},
+    {"variable": "xyz", "parents": ["v3d"], "support": model.xyz_point_cloud},
+    {"variable": ("ego_pos", "ego_matter"), "parents": ["xyz"], "support": model.egocentric_3d_map}
+    ]
+
+proposal_variables = [
+    {"variable": "ego_pos", "parents": [], "support": model.egocentric_3d_map},
+    {"variable": "xyz", "parents": ["ego_pos"],   "support": model.xyz_point_cloud},
+    {"variable": "v3d", "parents": ["xyz"], "support": model.xyz_vels},
+    {"variable": "diam", "parents": ["ego_pos"], "support": model.diams},
+    {"variable": "lights", "parents": [], "support": model.bool_support}]
+    
+obs_variables = [
+    {
+        "variable": "obs",
+        "parents": [],
+        "support": model.egocentric_2d_map
+    }
+]
--- packages/smcnn/core/src/genbrain_smcnn_core/interpreter/analog.py
+++ packages/smcnn/core/src/genbrain_smcnn_core/interpreter/analog.py
@@ -151,6 +151,29 @@ class Particle_Analog(Particle):
             v.state_buffer[str(state)].append(0.0)
 
 
+class ProbabilityMap(): 
+    def __init__(self, neurons_per_assembly, probability_array, support, distribution):
+        self.distribution = distribution
+        self.max_recursion_passes = 3
+        self.pp_length = 20
+        super().__init__(neurons_per_assembly, catprobs)
+        staterange = np.hstack((np.arange(self.num_states), np.arange(self.num_states)))
+        ps_qs = ["p"] * self.num_states + ["q"] * self.num_states
+        self.mux = {pq + str(s): [] for pq, s in zip(ps_qs, staterange)}
+        self.accum = {str(s): [] for s in np.arange(self.neurons_per_assembly)}
+        self.state_buffer = {str(s): [] for s in np.arange(self.num_states)}
+        self.component_dict["state_buffer"] = self.state_buffer
+        self.component_dict["accum"] = self.accum
+        self.component_dict["mux"] = self.mux
+
+        # update this at resample time (i.e. make state_buffer the resampled state for each particle
+        # in switch states.
+        #        self.kp = int(self.pp_length / 10)
+        self.kp = 40
+        self.kq = 10
+        self.population_λ = {"q": 0.3, "p": 0.3}
+
+
 class SampleScore_Analog(SampleScore_Base):
     def __init__(self, neurons_per_assembly, catprobs):
         self.num_pp_generated = {"p": 0, "q": 0}
